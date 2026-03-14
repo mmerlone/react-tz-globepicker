9b4c5790d069e221a58aec3b1abdbe25516615d1
Marcio Merlone <mmerlone@gmail.com>
2026-03-14 11:30:07 -0300

    add husky git hooks, types and workspace configuration

---
+++ types/global.d.ts
@@ -0,0 +1,28 @@
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+// Allow importing CSS files as side-effect modules in the demo
+declare module "*.css";
+declare module "*.module.css";
